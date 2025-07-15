a5155fe0695f45207ae221f1c696adf90bd5a290
Lucy <lucywang340@gmail.com>
2025-07-15 11:32:47 -0500

    welcome page

--- KLZRskin/ContentView.swift
+++ KLZRskin/ContentView.swift
@@ -9,16 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:1){
+            Image("Image")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 400.00, height: 200.0)
+                
+            Text("Welcome")
+                .font(.system(size:80))
+                .fontWeight(.heavy)
+                
+                .foregroundColor(Color(hue: 0.229, saturation: 0.251, brightness: 0.667))
+                .multilineTextAlignment(.center)
+            Text("to KLZR Glow")
+                .font(.system(size:40))
+            
+                .padding()
+             
+                Text("Our story:")
+                .font(.system(size:25))
+                .multilineTextAlignment(.leading)
+            
+                Text("We wanted to create an app that have unbiased advice about skincare. ")
+                .font(.system(size:20))
+                Text("")
+                 
+                
+              
+                    Text("About the app:")
+                    .font(.system(size:25))
+
+                    Text("Based on your skin type a  series of skincare prodcuts will be recommended.")
+                .font(.system(size:20))
+
+                    Text("")
+                    Text("")
+                    HStack{
+                        
+                        Text("Click to learn your skin type")
+                            .font(.system(size:20))
+
+                        Button("Click me") {
+                            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                        
+                        }
+                        .font(.system(size:20))
+                    }
+          
+            
         }
-        .padding()
+        Spacer()
     }
 }
-
 #Preview {
     ContentView()
 }
