162a4a8d3e05792c1670b0ef6fa588c1214ec674
Krisha Patel <patel.krisha.edu@gmail.com>
2025-07-15 11:23:19 -0500

    Initial Commit

---
+++ KLZRskin/KLZRskinApp.swift
@@ -0,0 +1,17 @@
+//
+//  KLZRskinApp.swift
+//  KLZRskin
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct KLZRskinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
