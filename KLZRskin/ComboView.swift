a378077282d2359bcac0738ebe3c48a6941bcd20
Lucy <lucywang340@gmail.com>
2025-07-17 10:04:41 -0500

    combo skni

--- KLZRskin/ComboView.swift
+++ KLZRskin/ComboView.swift
@@ -8,6 +8,10 @@ import SwiftUI
 struct comboSkinView: View {
 @State private var selectedProduct: String? = nil
 @State private var productName: String? = nil
+@State private var ShowImageComboMos=false
+@State private var ShowImageComboCleanser=false
+@State private var ShowImageComboSerum=false
+
  var body: some View {
   VStack{
     Text("Combo Skin")
@@ -24,14 +28,29 @@ struct comboSkinView: View {
     Button("Cleanser") {
      productName = "La Roche-Posay Tolerine Purifying Foaming Cleanser"
      selectedProduct = "This is a great cleanser if you have oily or combination skin and want something that does the job without stripping your face dry. It foams up nicely and gets rid of dirt, oil, and makeup, but still feels gentle, no tight or squeaky-clean feeling afterward. It’s also fragrance-free, which is a big plus for sensitive skin. The formula includes ingredients like niacinamide and the brand’s thermal spring water, which help calm the skin while cleansing. It’s a solid, no-nonsense daily face wash."
+        ShowImageComboSerum=false
+        ShowImageComboMos=false
+        ShowImageComboCleanser.toggle()
+        ShowImageComboCleanser=true
+
+
     }
     Button("Serum") {
      productName = "Paula’s Choice Skin Balancing Pore-Reducing Serum"
      selectedProduct = "This serum is a go-to if you deal with enlarged pores or uneven texture, especially in oily areas. It’s lightweight and sinks in quickly without leaving any sticky or greasy feel. The key ingredient, niacinamide, helps smooth out the skin, control shine, and reduce the appearance of pores over time. It’s easy to use under moisturizer or makeup, and it’s fragrance-free too, which makes it a safe option for sensitive skin."
+        ShowImageComboCleanser=false
+        ShowImageComboMos=false
+        ShowImageComboSerum.toggle()
+        ShowImageComboSerum=true
+
     }
     Button("Moisturizer") {
      productName = "Clinique Dramatically Different Hydrating Jelly "
      selectedProduct = "This jelly moisturizer is perfect if you want hydration without any heaviness. It’s clear, oil-free, and absorbs fast, great for people who don’t like the feeling of thick creams but still need moisture. It’s especially good for oily or acne-prone skin since it won’t clog pores or add shine. It also helps strengthen the skin’s barrier, which is key for keeping it healthy and resilient. You can use it alone or layer it under other skincare products easily."
+        ShowImageComboCleanser=false
+        ShowImageComboSerum=false
+        ShowImageComboMos.toggle()
+        ShowImageComboMos=true
     }
    }
    .padding()
@@ -47,6 +66,24 @@ struct comboSkinView: View {
      .padding()
     //alter how the description
    }
+      if ShowImageComboMos{
+          Image("comboMos")
+              .resizable(resizingMode: .stretch)
+                      .aspectRatio(contentMode: .fit)
+                       .frame(width: 150.0, height: 150.0)
+      }
+      if ShowImageComboCleanser{
+          Image("comboCleanser")
+              .resizable(resizingMode: .stretch)
+                      .aspectRatio(contentMode: .fit)
+                       .frame(width: 150.0, height: 150.0)
+      }
+      if ShowImageComboSerum{
+          Image("comboSerum")
+              .resizable(resizingMode: .stretch)
+                      .aspectRatio(contentMode: .fit)
+                       .frame(width: 150.0, height: 150.0)
+      }
   }
  }
 }
