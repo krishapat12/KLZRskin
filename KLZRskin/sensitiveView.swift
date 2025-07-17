7ae17a705ae69a05848ae53778feb1fc1f6370c3
Lucy <lucywang340@gmail.com>
2025-07-17 09:55:25 -0500

    sensitive skin

--- KLZRskin/sensitiveView.swift
+++ KLZRskin/sensitiveView.swift
@@ -8,6 +8,9 @@ import SwiftUI
 struct sensitiveView: View {
 @State private var selectedProduct: String? = nil
 @State private var productName: String? = nil
+@State private var ShowImageSensMos=false
+@State private var ShowImageSensCleanser=false
+@State private var ShowImageSensSerum=false
  var body: some View {
   VStack{
     Text("Sensitive Skin")
@@ -24,14 +27,28 @@ struct sensitiveView: View {
     Button("Cleanser") {
      productName = "La Roche-Posay Tolerine Hydrating Gentle Cleanser"
      selectedProduct = "This product cleanses the skin gently without causing irritation, making it ideal for those with sensitive or easily reactive skin. It’s free from fragrance and harsh ingredients, reducing the risk of dryness or discomfort. By maintaining the skin’s natural balance, it helps keep your complexion calm, clear, and healthy with every use."
+        ShowImageSensMos=false
+        ShowImageSensSerum=false
+        ShowImageSensCleanser.toggle()
+        ShowImageSensCleanser=true
     }
     Button("Serum") {
      productName = "The Ordinary Azelaic Acid Suspension 10%"
      selectedProduct = "This product is designed to help calm irritation while gently improving the appearance of your skin. It works to even out skin tone by fading dark spots over time, making it a great option for those looking to achieve a more radiant complexion. It's also a suitable alternative for individuals who are sensitive to retinoids or vitamin C, offering noticeable results without the risk of irritation."
+        ShowImageSensMos=false
+        ShowImageSensCleanser=false
+        ShowImageSensSerum.toggle()
+        ShowImageSensSerum=true
+
     }
     Button("Moisturizer") {
      productName = "Aveeno Ultra-Calming Daily Moisturizer SPF 30"
      selectedProduct = "This gentle daily moisturizer soothes and protects sensitive skin while defending against environmental stress. With broad-spectrum SPF 30, it shields skin from harmful UV rays without causing irritation. The non-greasy, fast-absorbing formula won’t clog pores, making it perfect for everyday wear."
+        ShowImageSensSerum=false
+        ShowImageSensCleanser=false
+        ShowImageSensMos.toggle()
+        ShowImageSensMos=true
+
     }
    }
    .padding()
@@ -47,6 +64,24 @@ struct sensitiveView: View {
      .padding()
     //alter how the description
    }
+      if ShowImageSensMos{
+        Image("sensMos")
+              .resizable(resizingMode: .stretch)
+                          .aspectRatio(contentMode: .fit)
+                           .frame(width: 150.0, height: 150.0)
+      }
+      if ShowImageSensCleanser{
+          Image("sensCleanser")
+              .resizable(resizingMode: .stretch)
+                          .aspectRatio(contentMode: .fit)
+                           .frame(width: 150.0, height: 150.0)
+      }
+      if ShowImageSensSerum{
+          Image("sensSerum")
+              .resizable(resizingMode: .stretch)
+                          .aspectRatio(contentMode: .fit)
+                           .frame(width: 150.0, height: 150.0)
+      }
   }
  }
 }
